052f25a84fb11e6d997b960b6304bb68f55b54e4
Itamar Souza <itasouza@yahoo.com.br>
2017-12-14 17:40:42 -0200

    Configuração Projeto Incial

---
+++ src/main/java/com/projeto/brewer/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.projeto.brewer.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import com.projeto.brewer.controller.CervejasController;
+
+@Configuration
+@ComponentScan(basePackageClasses = { CervejasController.class })
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+   
+	
+}
---
+++ src/main/java/com/projeto/brewer/controller/CervejasController.java
@@ -0,0 +1,8 @@
+package com.projeto.brewer.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller	
+public class CervejasController {
+
+}
